8d522c43f845e1c0fa9666c1d9e91ceeafa20014
Nicolas Castro Giraldo <nicolas.castro9219@gmail.com>
2021-11-07 14:29:13 -0500

    carga de repositorios

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,1 +1,6 @@
 export * from './proponente.repository';
+export * from './estado.repository';
+export * from './jurado.repository';
+export * from './juradoxsolicitud.repository';
+export * from './proponentexsolicitud.repository';
+export * from './solicitud.repository';
